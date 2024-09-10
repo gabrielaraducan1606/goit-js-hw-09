cf64304bc69c0de40b9f8bb7d16d50e2586718f6
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2024-09-11 00:23:11 +0300

    commit5

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,29 +4,33 @@ function createPromise(position, delay) {
 
     setTimeout(() => {
       if (shouldResolve) {
-        resolve({ position, delay });
+        resolve({ position, delay }); 
       } else {
-        reject({ position, delay });
+        reject({ position, delay }); 
       }
     }, delay);
   });
 }
 
-document.getElementById('promise-form').addEventListener('submit', (event) => {
-  event.preventDefault();
 
-  const delay = Number(event.target.elements.delay.value);
-  const step = Number(event.target.elements.step.value);
-  const amount = Number(event.target.elements.amount.value);
+function handleSubmit(event) {
+  event.preventDefault(); 
+  const delay = Number(event.target.delay.value);
+  const step = Number(event.target.step.value);
+  const amount = Number(event.target.amount.value);
 
-  for (let i = 0; i < amount; i++) {
-    const currentDelay = delay + i * step;
-    createPromise(i + 1, currentDelay)
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = delay + (i - 1) * step;
+
+    createPromise(i, currentDelay)
       .then(({ position, delay }) => {
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
       })
       .catch(({ position, delay }) => {
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
       });
   }
-});
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', handleSubmit);
